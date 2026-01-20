63f93468f3656c46230caec77051ae8825f40c96
gng <you@example.com>
2026-01-20 19:22:26 +0100

    Remove temporary debug scripts
    Removed:
    - add_traces.sh
    - install-32bit-debug.sh
    - test-reaper-32bit-vtable.sh
    
    These were used during the 32-bit debugging process and are no longer needed.
    
    Kept:
    - test_asio_minimal.c/.exe - Useful diagnostic tool for users
    - package.sh - Release packaging script

--- add_traces.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-# Add TRACE to beginning of each asio_ function
-sed -i '
-/^static NTSTATUS asio_.*void \*args)$/,/^{$/ {
-  /^{$/a\    TRACE("%s called\\n", __func__);
-}
-' asio_unix.c
